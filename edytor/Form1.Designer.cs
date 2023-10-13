7f89c7ee9173b4a877f33a92ac7d7aef2ee81936
Hlib Khorunzhyi <gleb5192005@gmai.com>
2023-10-13 22:11:57 +0200

    first commit

---
+++ edytor/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace edytor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNone = new System.Windows.Forms.Button();
+            this.btnSecret = new System.Windows.Forms.Button();
+            this.colorBox = new System.Windows.Forms.ComboBox();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnBlack = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.btnSquare = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnElipse = new System.Windows.Forms.Button();
+            this.btnLine = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnNone
+            // 
+            this.btnNone.Image = global::edytor.Properties.Resources.none;
+            this.btnNone.Location = new System.Drawing.Point(12, 12);
+            this.btnNone.Name = "btnNone";
+            this.btnNone.Size = new System.Drawing.Size(120, 60);
+            this.btnNone.TabIndex = 0;
+            this.btnNone.UseVisualStyleBackColor = true;
+            this.btnNone.Click += new System.EventHandler(this.btnNone_Click);
+            // 
+            // btnSecret
+            // 
+            this.btnSecret.Enabled = false;
+            this.btnSecret.Location = new System.Drawing.Point(1244, 12);
+            this.btnSecret.Name = "btnSecret";
+            this.btnSecret.Size = new System.Drawing.Size(75, 23);
+            this.btnSecret.TabIndex = 4;
+            this.btnSecret.Text = "button1";
+            this.btnSecret.UseVisualStyleBackColor = true;
+            this.btnSecret.Click += new System.EventHandler(this.btnSecret_Click);
+            // 
+            // colorBox
+            // 
+            this.colorBox.FormattingEnabled = true;
+            this.colorBox.Items.AddRange(new object[] {
+            "red",
+            "blue",
+            "green",
+            "yellow",
+            "black"});
+            this.colorBox.Location = new System.Drawing.Point(1244, 41);
+            this.colorBox.Name = "colorBox";
+            this.colorBox.Size = new System.Drawing.Size(85, 28);
+            this.colorBox.TabIndex = 5;
+            this.colorBox.Tag = "";
+            this.colorBox.Text = "red";
+            this.colorBox.SelectedIndexChanged += new System.EventHandler(this.colorBox_SelectedIndexChanged);
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.Location = new System.Drawing.Point(605, 12);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(60, 60);
+            this.btnRed.TabIndex = 6;
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.BackColor = System.Drawing.Color.Blue;
+            this.btnBlue.Location = new System.Drawing.Point(671, 13);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(60, 60);
+            this.btnBlue.TabIndex = 7;
+            this.btnBlue.UseVisualStyleBackColor = false;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.BackColor = System.Drawing.Color.Lime;
+            this.btnGreen.Location = new System.Drawing.Point(737, 13);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(60, 60);
+            this.btnGreen.TabIndex = 8;
+            this.btnGreen.UseVisualStyleBackColor = false;
+            this.btnGreen.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.BackColor = System.Drawing.Color.Yellow;
+            this.btnYellow.Location = new System.Drawing.Point(803, 12);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(60, 60);
+            this.btnYellow.TabIndex = 9;
+            this.btnYellow.UseVisualStyleBackColor = false;
+            this.btnYellow.Click += new System.EventHandler(this.btnYellow_Click);
+            // 
+            // btnBlack
+            // 
+            this.btnBlack.BackColor = System.Drawing.Color.Black;
+            this.btnBlack.Location = new System.Drawing.Point(869, 12);
+            this.btnBlack.Name = "btnBlack";
+            this.btnBlack.Size = new System.Drawing.Size(60, 60);
+            this.btnBlack.TabIndex = 10;
+            this.btnBlack.UseVisualStyleBackColor = false;
+            this.btnBlack.Click += new System.EventHandler(this.btnBlack_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(948, 13);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(220, 69);
+            this.trackBar1.TabIndex = 12;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // btnSquare
+            // 
+            this.btnSquare.Image = global::edytor.Properties.Resources.Rectangle;
+            this.btnSquare.Location = new System.Drawing.Point(449, 12);
+            this.btnSquare.Name = "btnSquare";
+            this.btnSquare.Size = new System.Drawing.Size(146, 60);
+            this.btnSquare.TabIndex = 11;
+            this.btnSquare.UseVisualStyleBackColor = true;
+            this.btnSquare.Click += new System.EventHandler(this.btnSquare_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 88);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1317, 759);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // btnElipse
+            // 
+            this.btnElipse.Image = global::edytor.Properties.Resources.elipse;
+            this.btnElipse.Location = new System.Drawing.Point(297, 12);
+            this.btnElipse.Name = "btnElipse";
+            this.btnElipse.Size = new System.Drawing.Size(146, 60);
+            this.btnElipse.TabIndex = 2;
+            this.btnElipse.UseVisualStyleBackColor = true;
+            this.btnElipse.Click += new System.EventHandler(this.btnElipse_Click);
+            // 
+            // btnLine
+            // 
+            this.btnLine.Image = global::edytor.Properties.Resources.line;
+            this.btnLine.Location = new System.Drawing.Point(138, 12);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(153, 60);
+            this.btnLine.TabIndex = 1;
+            this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1341, 859);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btnSquare);
+            this.Controls.Add(this.btnBlack);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnRed);
+            this.Controls.Add(this.colorBox);
+            this.Controls.Add(this.btnSecret);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnElipse);
+            this.Controls.Add(this.btnLine);
+            this.Controls.Add(this.btnNone);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Paint";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNone;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnElipse;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSecret;
+        private System.Windows.Forms.ComboBox colorBox;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnBlack;
+        private System.Windows.Forms.Button btnSquare;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ edytor/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace edytor
+{
+    public partial class Form1 : Form
+    {
+        enum Tools {None, Line, Elipse, Square};
+        Tools tool = Tools.None;
+        Graphics graphics;
+        Pen pen = new Pen(Color.Red,1);
+        int x0, y0,x1,y1;
+        bool mouseButton = false;
+        Bitmap bitmap;
+        //public ToolStripComboBox("red");
+
+        public Form1()
+        {
+            InitializeComponent();
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(bitmap);
+
+
+        }
+
+        private void btnNone_Click(object sender, EventArgs e)
+        {
+            tool = Tools.None;
+            //graphics.Clear();
+        }
+
+        private void btnLine_Click(object sender, EventArgs e)
+        {
+            tool = Tools.Line;
+            //graphics.DrawLine(pen, 10,10,100,100);
+        }
+
+        private void btnElipse_Click(object sender, EventArgs e)
+        {
+            tool = Tools.Elipse;
+            //graphics.DrawEllipse(pen, 100,100,50,50);
+        }
+        private void btnSquare_Click(object sender, EventArgs e)
+        {
+            tool = Tools.Square;
+        }
+
+        
+    
+        private void colorBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string item = colorBox.SelectedItem.ToString();
+            switch (item)
+            {
+                case "red": pen.Color = Color.Red; break;
+                case "blue": pen.Color = Color.Blue; break;
+                case "green": pen.Color = Color.Green; break;
+                case "yellow": pen.Color = Color.Yellow; break;
+                case "black": pen.Color = Color.Black; break;
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (tool != Tools.None && Control.MouseButtons == MouseButtons.Left)
+            {
+                mouseButton = true;
+                x0 = e.X; y0 = e.Y;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((tool != Tools.None) && (mouseButton))
+            {
+                x1 = e.X;
+                y1 = e.Y;
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if ((tool != Tools.None) && mouseButton)
+            {
+                switch (tool)
+                {
+                    case Tools.Line:
+                        graphics.DrawLine(pen, x0, y0, e.X, e.Y);
+                        break;
+                    case Tools.Elipse:
+                        graphics.DrawEllipse(pen, x0, y0, e.X - x0, e.Y - y0);
+                        break;
+                    case Tools.Square:
+                        graphics.DrawRectangle(pen, x0, y0, Math.Abs(x0 - x1), Math.Abs(y0 - y1));
+                        break;
+                }
+                mouseButton = false;
+                pictureBox1.Image = bitmap;
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if ((tool != Tools.None) && mouseButton)
+            {
+                switch (tool)
+                {
+                    case Tools.Line:
+                        e.Graphics.DrawLine(pen, x0, y0, x1, y1);
+                        break;
+                    case Tools.Elipse:
+                        e.Graphics.DrawEllipse(pen, x0, y0, x1 - x0, y1 - y0);
+                        break;
+                    case Tools.Square:
+                        int minx = Math.Min(x0, x1);
+                        int miny = Math.Min(y0, y1);
+                        int width = Math.Abs(x0 - y1);
+                        int height = Math.Abs(y0 - x1);
+                        e.Graphics.DrawRectangle(pen, minx, miny, width, height);
+                        break;
+                }
+            }
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Red;
+        }
+
+        private void btnYellow_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Yellow;
+        }
+
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Green;
+        }
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Blue;
+        }
+
+        private void btnBlack_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Black;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            pen.Width = trackBar1.Value;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+
+        private void btnSecret_Click(object sender, EventArgs e)
+        {
+            graphics.DrawEllipse(pen, 300, 310, 100, 100);
+            graphics.DrawEllipse(pen, 400, 310, 100, 100);
+            graphics.DrawEllipse(pen, 350, 100, 100, 230);
+            graphics.DrawLine(pen, 363, 140, 437, 140);
+            graphics.DrawLine(pen, 400, 100, 400, 140);
+        }
+    }
+}
+
